5f6956ffc32d6ead634d125acc5a8d6a45b183d7
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-01 13:02:31 -0600

    Updating v fn

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,4 +1,9 @@
-vim.opt.termguicolors = true
+local set = vim.opt
+
+set.termguicolors = true
+
+set.autoindent = true
+set.cursorline = true
 
 set.tabstop = 4
 set.shiftwidth = 4
--- dot_config/private_fish/functions/v.fish
+++ dot_config/private_fish/functions/v.fish
@@ -1,3 +1,9 @@
-function v --description 'Opens nvim'
-nvim
+function v --description 'Opens nvim with optional args [$argv]'
+    set file_to_open $argv[1]
+
+    if test -z $file_to_open
+        nvim
+    end
+
+    nvim $file_to_open
 end

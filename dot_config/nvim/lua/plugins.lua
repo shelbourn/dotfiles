799517e95420c39b7a7be58996b9035ea790d65c
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-03 03:10:59 -0600

    Adding telescope

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,13 +1,19 @@
 local vim = vim
 local Plug = vim.fn['plug#']
 
+-- plugins [start]
 vim.call('plug#begin')
 
--- plugins [start]
+-- lsp
 Plug('rust-lang/rust.vim')
 Plug('neovim/nvim-lspconfig')
 Plug('williamboman/mason.nvim')
 Plug('williamboman/mason-lspconfig.nvim')
+Plug('mrcjkb/rustaceanvim', {
+    ['tag'] = '4'
+})
+
+-- completion
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('hrsh7th/cmp-buffer')
@@ -16,24 +22,34 @@ Plug('hrsh7th/cmp-cmdline')
 Plug('saadparwaiz1/cmp_luasnip')
 Plug('L3MON4D3/LuaSnip')
 Plug('rafamadriz/friendly-snippets')
-Plug('mrcjkb/rustaceanvim', {
-    ['tag'] = '4'
-})
+
+-- status line
 Plug('konapun/vacuumline.nvim')
 Plug('glepnir/galaxyline.nvim', {
     ['branch'] = 'main'
 })
 Plug('nvim-tree/nvim-web-devicons')
 Plug('folke/lsp-colors.nvim')
+
+-- telescope
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', {
+    ['tag'] = '0.1.5'
+})
+Plug('BurntSushi/ripgrep')
+Plug('vim-scripts/telescope-fzf-native.nvim')
+Plug('sharkdp/fd')
+
+-- diagnostics
 Plug('folke/trouble.nvim', {
     ['dependencies'] = 'nvim-tree/nvim-web-devicons'
 })
-
 
 -- theme
 Plug('rebelot/kanagawa.nvim')
 
 vim.call('plug#end')
+-- plugins end
 
 -- status line theme
 require('vacuumline').setup({

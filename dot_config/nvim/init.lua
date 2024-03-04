9eddcc6a039a4aa403de49a7897b1ea8f2be2c46
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-04 17:26:57 -0600

    removing commented code

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,14 +12,6 @@ require('plugin-config')
 -- language server initialization
 require('lang-servers')
 
--- telescope initialization
--- require('telescope').setup()
--- local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 -- syntax config
 vim.cmd('syntax on')
 vim.cmd('filetype plugin indent on')

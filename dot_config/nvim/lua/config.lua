7553e0dc7d9b90cbfbafc1377b5fb1bcce3674be
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-06 16:58:58 -0600

    adding active/inactive splits coloring

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -4,11 +4,12 @@ local keymap = vim.keymap.set
 local cmd = vim.cmd
 local api = vim.api
 
+-- term colors
 set.termguicolors = true
 
+-- tab width, indentation, cursor
 set.autoindent = true
 set.cursorline = true
-
 set.tabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
@@ -26,7 +27,7 @@ set.clipboard = 'unnamed'
 cmd('syntax on')
 cmd('filetype plugin indent on')
 -----
---keybinds
+-- keybinds
 -----
 
 -- leaders
@@ -36,18 +37,37 @@ global.maplocalleader = ' '
 -- deselect (:noh)
 keymap('n', '<Leader>noh', vim.cmd.noh)
 
--- Yank into system clipboard
+-- yank into system clipboard
 keymap({'n', 'v'}, '<leader>y', '"+y') -- yank motion
 keymap({'n', 'v'}, '<leader>Y', '"+Y') -- yank line
 
--- Delete into system clipboard
+-- delete into system clipboard
 keymap({'n', 'v'}, '<leader>d', '"+d') -- delete motion
 keymap({'n', 'v'}, '<leader>D', '"+D') -- delete line
 
--- Paste from system clipboard
+-- paste from system clipboard
 keymap('n', '<leader>p', '"+p')  -- paste after cursor
 keymap('n', '<leader>P', '"+P')  -- paste before cursor
 
--- Inserting lines above/below without leaving normal mode
+-- inserting lines above/below without leaving normal mode
 keymap('n', '<leader>o', 'o<Esc>')  -- insert line below
 keymap('n', '<leader>O', 'O<Esc>')  -- insert line above
+
+-- active/inactive window highlights
+
+-- background colors for active vs inactive windows
+cmd([[
+hi ActiveWindow guibg=#17252c
+hi InactiveWindow guibg=#0D1B22
+
+" Call method on window enter
+augroup WindowManagement
+  autocmd!
+  autocmd WinEnter * call Handle_Win_Enter()
+augroup END
+
+" Change highlight group of active/inactive windows
+function! Handle_Win_Enter()
+  setlocal winhighlight=Normal:ActiveWindow,NormalNC:InactiveWindow
+endfunction
+]])

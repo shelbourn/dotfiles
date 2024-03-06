509f8b5388623c8502c6a7e2b2673b99261b1126
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-06 09:51:22 -0600

    updating theme

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,3 +6,9 @@ pyenv init - | source
 
 # Sets the auto-complete font color
 set -g fish_color_autosuggestion 92dd94
+
+# theming
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme zenburn
+set -g theme_display_git_default_branch yes
+set -g theme_display_hostname yes

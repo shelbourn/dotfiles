e3df328a41113df3bfce2e1e45fb310dcb6542e8
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-09 23:53:34 -0600

    RENAMING ~/nvim to ~/nvim-shelly-ryo

---
+++ dot_config/nvim-shelly-ryo/lua/plugins.lua
@@ -0,0 +1,58 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+-- plugins [start]
+vim.call('plug#begin')
+
+
+-- completion
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('saadparwaiz1/cmp_luasnip')
+Plug('L3MON4D3/LuaSnip')
+Plug('rafamadriz/friendly-snippets')
+
+-- diagnostics
+Plug('folke/trouble.nvim', {
+    ['dependencies'] = 'nvim-tree/nvim-web-devicons'
+})
+
+-- formatting
+Plug('prettier/vim-prettier', {
+    ['do'] = 'yarn install --frozen-lockfile --production',
+    ['for'] = {'javascript', 'typescript', 'css', 'less', 'scss', 'json', 'graphql', 'markdown', 'vue', 'svelte', 'yaml', 'html'}
+})
+
+-- git
+Plug('tpope/vim-fugitive')
+
+-- lsp
+Plug('rust-lang/rust.vim')
+Plug('neovim/nvim-lspconfig')
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+Plug('mrcjkb/rustaceanvim', {
+    ['tag'] = '4*'
+})
+Plug('folke/lsp-colors.nvim')
+
+-- status line
+Plug('nvim-lualine/lualine.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+
+-- telescope
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', {
+    ['tag'] = '0.1.x'
+})
+Plug('nvim-telescope/telescope-fzf-native.nvim')
+
+-- theme
+Plug('rebelot/kanagawa.nvim')
+
+vim.call('plug#end')
+-- plugins end
+

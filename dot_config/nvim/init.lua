81666efa767a782f5df7f01cfba110fa4dce3dca
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-09 23:50:35 -0600

    RE-ADDING SHELLY NVIM CONFIG FOR VCS (DIDN'T KNOW IT WOULD DELETE FROM REPO WITH "FORGET" FUNCTION)

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,13 @@
+local vim = vim
+
+-- config option initialization
+require('config')
+
+-- plugin initialization
+require('plugins')
+
+-- plugin config and keybinds
+require('plugin-config')
+
+-- language server initialization
+require('lang-servers')

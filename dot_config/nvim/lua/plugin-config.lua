5e7384ba7a95adea2b044c98bba957f8d9baf379
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-09 23:48:21 -0600

    LAST UPDATE PRIOR TO CONVERSION TO NVCHAD

--- dot_config/nvim/lua/plugin-config.lua
+++ dot_config/nvim/lua/plugin-config.lua
@@ -78,13 +78,14 @@ keymap('n', '<leader>ff', builtin.find_files, {})
 keymap('n', '<leader>fg', builtin.live_grep, {})
 keymap('n', '<leader>fb', builtin.buffers, {})
 keymap('n', '<leader>fh', builtin.help_tags, {})
+keymap('n', '<leader>sd', builtin.grep_string, { silent = true, noremap = true })
 
 local actions = require("telescope.actions")
 require("telescope").setup{
   defaults = {
     mappings = {
       i = {
-        ["<esc>"] = actions.close
+        ["<esc>"] = actions.close,
       },
     },
   }

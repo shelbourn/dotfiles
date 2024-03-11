644e6062580087c415f03ff9442bbf00557b4b4a
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-11 11:40:01 -0500

    updating kub fns

--- dot_config/private_fish/functions/kub.fish
+++ dot_config/private_fish/functions/kub.fish
@@ -1,12 +1,12 @@
 function kub --description 'alias for kubectx + args'
     set kubectx_args $argv
 
-    # if an argument does not  exist then exec kubectx as a standalone
-    if test -z $kubectx_args
-        kubectx
+    # if an argument(s) does  exist then pass it(them) to kubctx
+    if test -n $kubectx_args
+        kubectx $kubectx_args
         return
     end
 
-    # if an argument does exist then pass it to kubectx
-    kubectx $kubectx_args
+    # if an argument does not exist then exec kubectx as a standalone
+    kubectx
 end
--- dot_config/private_fish/functions/kubl.fish
+++ dot_config/private_fish/functions/kubl.fish
@@ -1,12 +1,12 @@
 function kubl --description 'alias for kubectl + args'
     set kubectl_args $argv
 
-    # if an argument does not  exist then exec kubectl as a standalone
-    if test -z $kubectl_args
-        kubectl
+    # if an argument(s) does exist then pass it(them) to kubectl
+    if test -n $kubectl_args
+        kubectl $kubctl_args
         return
     end
 
-    # if an argument does exist then pass it to kubectl
-    kubectl $kubectl_args
+    # if an argument does not exist then exec kubectl as a standalone
+    kubectl
 end

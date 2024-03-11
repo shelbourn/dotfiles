b07cfec976f96c2362987ca6e982753392085fa8
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-11 11:21:35 -0500

    adding kubctx fns

--- dot_config/private_fish/functions/kub.fish
+++ dot_config/private_fish/functions/kub.fish
@@ -1,3 +1,12 @@
-function kub --description 'alias for kubectx'
-kubectx
+function kub --description 'alias for kubectx + args'
+    set kubectx_arg $argv[1]
+
+    # if an argument does not  exist then exec kubectx as a standalone
+    if test -z $kubectx_arg
+        kubectx
+        return
+    end
+
+    # if an argument does exist then pass it to kubectx
+    kubectx $kubectx_arg
 end
--- dot_config/private_fish/functions/kubl.fish
+++ dot_config/private_fish/functions/kubl.fish
@@ -1,3 +1,12 @@
-function kubl --description 'alias for kubectl'
-kubectl
+function kubl --description 'alias for kubectl + args'
+    set kubectl_arg $argv[1]
+
+    # if an argument does not  exist then exec kubectl as a standalone
+    if test -z $kubectl_arg
+        kubectl
+        return
+    end
+
+    # if an argument does exist then pass it to kubectl
+    kubectl $kubectl_arg
 end

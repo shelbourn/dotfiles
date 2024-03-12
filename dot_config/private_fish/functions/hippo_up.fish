dca37e5cc75601fec2e9a4d1f00f70052430df89
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-11 23:39:47 -0500

    refactoring fns

--- dot_config/private_fish/functions/hippo_up.fish
+++ dot_config/private_fish/functions/hippo_up.fish
@@ -1,3 +1,3 @@
 function hippo_up --description 'Docker up with Mirage, dependency installs/updates, minified JS, python debugging enabled'
-    ENABLE_MIRAGE_MOCK=true SKIP_INSTALL=true docker compose -f docker-compose.yml up -d --remove-orphans
+    ENABLE_MIRAGE_MOCK=True SKIP_LEGACY_FE_BUILD=False SKIP_INSTALL=True docker compose -f docker-compose.yml up -d --remove-orphans
 end
--- dot_config/private_fish/functions/hippo_up_legacy.fish
+++ dot_config/private_fish/functions/hippo_up_legacy.fish
@@ -1,3 +1,3 @@
 function hippo_up_legacy --description 'Docker compose up with Mirage, un-minified JS, and python debugging enabled'
-    ENABLE_MIRAGE_MOCK=true MINIFY_JS=false SKIP_INSTALL=true docker compose up -d
+    ENABLE_MIRAGE_MOCK=True MINIFY_JS=False SKIP_LEGACY_FE_BUILD=False SKIP_INSTALL=True docker compose up -d
 end

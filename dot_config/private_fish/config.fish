e35de069aa30b33cae48b415fbd9d4ee3c37e512
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-03-15 21:10:50 -0500

    removing kubectx/kubectl fns

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+kubectl completion fish | source
 pyenv init - | source
 
 # Sets the auto-complete font color

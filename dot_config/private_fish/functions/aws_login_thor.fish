27cf36f5911033d96e0ae23506f147b049ccb0d8
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-04-02 13:00:09 -0500

    Updating aws_login_thor to use thor_engineering profile

--- dot_config/private_fish/functions/aws_login_thor.fish
+++ dot_config/private_fish/functions/aws_login_thor.fish
@@ -1,5 +1,5 @@
 function aws_login_thor --description 'AWS SSO  authentication for LightningMD (Thor)'
-    aws sso login --profile thor_admin
-    aws ecr get-login-password --profile thor_admin --region us-east-1 | docker login --username AWS --password-stdin 570488747013.dkr.ecr.us-east-1.amazonaws.com
+    aws sso login --profile thor_engineering
+    aws ecr get-login-password --profile thor_engineering --region us-east-1 | docker login --username AWS --password-stdin 570488747013.dkr.ecr.us-east-1.amazonaws.com
     echo "Thor AWS authentication successful"
 end

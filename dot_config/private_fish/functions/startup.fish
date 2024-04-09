7e4b675410bb8637bfc6911a30cbbb9e45bb6538
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-04-09 13:50:20 -0500

    updating some fns

--- dot_config/private_fish/functions/startup.fish
+++ dot_config/private_fish/functions/startup.fish
@@ -4,10 +4,10 @@ function startup --description 'Executes a collection of startup scripts'
     aws_login_thor
     mysql_env
     # hippo
-    thor
-    remote_container_patch
-    untrack
-    untrack .devcontainer/devcontainer.json
+    # thor
+    # remote_container_patch
+    # untrack
+    # untrack .devcontainer/devcontainer.json
     # untrack elation/elation_billing/services/api_proxy.py
     # untrack elation/elation_billing/services/auth.py
     pyenv shell 3.6.15

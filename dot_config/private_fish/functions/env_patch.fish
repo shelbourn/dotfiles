005544779cf3892886b4b76e6836f8624b08cbad
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-06-08 14:02:36 -0500

    Adding function for applying a hippo .env patch

---
+++ dot_config/private_fish/functions/env_patch.fish
@@ -0,0 +1,3 @@
+function env_patch --description 'Applies git patch for local hippo .env file which includes Docker startup vars'
+    patch ~/Desktop/Working\ Files/Git\ PATCH\ Files/hippo-env.patch
+end

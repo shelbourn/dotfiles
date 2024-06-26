100e0ee3a36803f71c794bd44b1358225f7e5077
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-06-26 12:18:54 -0500

    renaming git_new fn

---
+++ dot_config/private_fish/functions/chrome_no_ext.fish
@@ -0,0 +1,4 @@
+function chrome_no_ext --description 'opens an instance of Chrome without extensions enabled'
+open --new -a "Google Chrome" --args --disable-extensions
+echo "open --new -a "Google Chrome" --args --disable-extensions"
+end
---
+++ dot_config/private_fish/functions/new.fish
@@ -0,0 +1,10 @@
+function new --description 'Creates a new Git branch using the passed in argument (spaces replaced with hyphens) as the branch name'
+    # Assigns the provided branch name (argument) to a var
+    set raw_branch_name $argv
+
+    # Parses the provided branch name and replaces ' ' with '-' then assigns it to a var
+    set formatted_branch_name (string replace -a ' ' - $raw_branch_name)
+
+    # Creates the new branch with the formatted branch name
+    git checkout -b $formatted_branch_name
+end

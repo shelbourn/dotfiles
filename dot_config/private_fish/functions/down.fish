48479f6c53fc5005d509bacd407cfe64751fe6cd
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-07-05 14:04:09 -0500

    updating down function

--- dot_config/private_fish/functions/down.fish
+++ dot_config/private_fish/functions/down.fish
@@ -1,3 +1,10 @@
 function down --description 'Docker compose down'
-    docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml down
+    if test $argv[1] && test $argv[1] = hippo
+        echo "Docker composing down hippo and event streaming containers"
+        docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml down
+        return
+    end
+
+    echo "Docker composing down containers"
+    docker compose -f docker-compose.yml down
 end

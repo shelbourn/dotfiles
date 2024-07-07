db0b343149c89a41fa0f1fe0ad070f42518976a1
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-07-06 23:47:02 -0500

    updating pause / unpause functions

--- dot_config/private_fish/functions/pause.fish
+++ dot_config/private_fish/functions/pause.fish
@@ -1,3 +1,10 @@
 function pause --description 'Pauses all running Docker containers'
-docker compose pause
+    if test $argv[1] && test $argv[1] = hippo
+        echo "Pausing hippo and event streaming containers"
+        docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml pause
+        return
+    end
+
+    echo "Pausing thor containers"
+    docker compose pause
 end
--- dot_config/private_fish/functions/unpause.fish
+++ dot_config/private_fish/functions/unpause.fish
@@ -1,3 +1,10 @@
 function unpause --description 'Unpauses all paused Docker containers'
-docker compose unpause
+    if test $argv[1] && test $argv[1] = hippo
+        echo "Unpausing hippo and event streaming containers"
+        docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml unpause
+        return
+    end
+
+    echo "Unpausing thor containers"
+    docker compose unpause
 end

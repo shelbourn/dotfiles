3e5af9325f57f70d9616efc59a710dd76b6a10e2
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-07-12 19:42:11 -0500

    Updating fns

--- dot_config/private_fish/functions/startup.fish
+++ dot_config/private_fish/functions/startup.fish
@@ -1,7 +1,7 @@
 function startup --description 'Executes a collection of startup scripts'
     aws_login_1
     # aws_login_2
-    aws_login_thor
+    # aws_login_thor
     mysql_env
     # hippo
     # thor

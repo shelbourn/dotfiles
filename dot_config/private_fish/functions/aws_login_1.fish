8015f2ccc4150fe23783b67796fe49c73c5eec94
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-07-12 20:17:35 -0500

    updating fns

--- dot_config/private_fish/functions/aws_login_1.fish
+++ dot_config/private_fish/functions/aws_login_1.fish
@@ -1,5 +1,6 @@
 function aws_login_1 --description 'Automatically executes the entire login process for AWS code artifact on US-WEST-1. Needed for working on Hippo'
     aws_dock
     loginwest1
+    loginwest2
     pypi
 end

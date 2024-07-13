73a6a5c09d044b079c224e03ba5e0cb037c1b2e5
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-07-12 20:32:34 -0500

    Updating fns

--- dot_config/private_fish/functions/up.fish
+++ dot_config/private_fish/functions/up.fish
@@ -1,14 +1,14 @@
 function up --description 'docker compose up hippo, thor-web, event-streaming'
     if test $argv[1] && test $argv[1] = hippo
         echo "Docker composing up hippo and event streaming"
-        echo "ENABLE_MIRAGE_MOCK=True SKIP_INSTALL=True MINIFY_JS=False SKIP_LEGACY_FE_BUILD=False docker compose -f docker-compose.yml up -d --remove-orphans"
+        echo "ENABLE_MIRAGE_MOCK=True SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml up -d --remove-orphans"
         ENABLE_MIRAGE_MOCK=True SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml up -d --remove-orphans
         return
     end
 
     if test $argv[1] && test $argv[1] = event
         echo "Docker composing up hippo and event streaming"
-        echo "ENABLE_MIRAGE_MOCK=True SKIP_INSTALL=True MINIFY_JS=False SKIP_LEGACY_FE_BUILD=False docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml up -d --remove-orphans"
+        echo "ENABLE_MIRAGE_MOCK=True SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml up -d --remove-orphans"
         ENABLE_MIRAGE_MOCK=True SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml up -d --remove-orphans
         return
     end

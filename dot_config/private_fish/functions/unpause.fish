516eae88201c4ec5be9ee2e23bf7d1bdbc67e27e
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-07-14 20:06:53 -0500

    updating fns

--- dot_config/private_fish/functions/unpause.fish
+++ dot_config/private_fish/functions/unpause.fish
@@ -1,12 +1,20 @@
 function unpause --description 'Unpauses all paused Docker containers'
     if test $argv[1] && test $argv[1] = hippo
+        echo "Unpausing hippo containers"
+        echo "docker compose -f docker-compose.yml unpause"
+        docker compose -f docker-compose.yml unpause
+        return
+    end
+
+    if test $argv[1] && test $argv[1] = event
         echo "Unpausing hippo and event streaming containers"
         echo "docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml unpause"
         docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml unpause
         return
     end
 
-    echo "Unpausing thor containers"
+    echo "Unpausing Docker containers"
     echo "docker compose unpause"
     docker compose unpause
+
 end

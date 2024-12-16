1b6e57671b632a845a6e561f0537a9fa91a66274
shelbourn <11466465+shelbourn@users.noreply.github.com>
2024-12-16 11:20:28 -0600

    updating up fn

--- dot_config/private_fish/functions/up.fish
+++ dot_config/private_fish/functions/up.fish
@@ -13,7 +13,15 @@ function up --description 'docker compose up hippo, thor-web, event-streaming'
         return
     end
 
+    if test $argv[1] && test $argv[1] = clean
+        echo "Docker composing up hippo and event streaming containers with no MIRAGE MOCK"
+        echo "SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml up -d --remove-orphans"
+        SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml up -d --remove-orphans
+        return
+    end
+
     echo "Docker composing up containers"
     echo "docker compose -f docker-compose.yml up -d --remove-orphans"
     docker compose -f docker-compose.yml up -d --remove-orphans
+
 end

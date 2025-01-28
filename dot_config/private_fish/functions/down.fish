e4e4cf75f2700bb4c577417b5a2d5c372b89010f
shelbourn <11466465+shelbourn@users.noreply.github.com>
2025-01-28 16:03:46 -0600

    udpating down fn

--- dot_config/private_fish/functions/down.fish
+++ dot_config/private_fish/functions/down.fish
@@ -1,6 +1,6 @@
 function down --description 'Docker compose down'
-    if test $argv[1] && test $argv[1] = hippo
-        echo "Docker composing down hippo containers"
+    if test $argv[1] && test $argv[1] = thor
+        echo "Docker composing down thor containers"
         echo "docker compose -f docker-compose.yml down"
         docker compose -f docker-compose.yml down
         return

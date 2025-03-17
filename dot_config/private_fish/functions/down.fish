cb316511683c588370764eaf55ff996a4926b305
shelbourn <11466465+shelbourn@users.noreply.github.com>
2025-03-17 16:05:05 -0500

    updating down fn

--- dot_config/private_fish/functions/down.fish
+++ dot_config/private_fish/functions/down.fish
@@ -1,14 +1,16 @@
 function down --description 'Docker compose down'
-    if test $argv[1] && test $argv[1] = thor
-        echo "Docker composing down thor containers"
-        echo "docker compose -f docker-compose.yml down"
-        docker compose -f docker-compose.yml down
-        return
+    set -l target_path ~/Projects/Elation/hippo
+    set -l current_dir (pwd)
+
+    if test "$current_dir" = "$target_path"
+        echo "Currently working in the Hippo repo"
+        echo "Docker composing down Hippo with Event-Streaming"
+        echo "Executing: docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml down"
+        docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml down
+    else
+        echo "Currently working in another repo other than Hippo"
+        echo "Docker composing down with docker-compose.yaml from current working directory"
+        echo "Docker compose down"
+        docker compose down
     end
-
-    echo "Docker composing down hippo and event streaming containers"
-    echo "docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml  down"
-    docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml down
-    return
-
 end

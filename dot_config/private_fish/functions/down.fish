f979969c1f0b2deb841fe604c7e5de8101a89528
shelbourn <11466465+shelbourn@users.noreply.github.com>
2025-03-20 12:12:59 -0500

    updating fns

--- dot_config/private_fish/functions/down.fish
+++ dot_config/private_fish/functions/down.fish
@@ -3,12 +3,12 @@ function down --description 'Docker compose down'
     set -l current_dir (pwd)
 
     if test "$current_dir" = "$target_path"
-        echo "Currently working in the Hippo repo"
+        echo "Currently working in the hippo repo"
         echo "Docker composing down Hippo with Event-Streaming"
         echo "Executing: docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml down"
         docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml down
     else
-        echo "Currently working in another repo other than Hippo"
+        echo "Currently working in the thor-web repo"
         echo "Docker composing down with docker-compose.yaml from current working directory"
         echo "Docker compose down"
         docker compose down
--- dot_config/private_fish/functions/up.fish
+++ dot_config/private_fish/functions/up.fish
@@ -3,12 +3,12 @@ function up --description 'docker compose up hippo, thor, event-streaming'
     set -l current_dir (pwd)
 
     if test "$current_dir" = "$target_path"
-        echo "Currently working in the Hippo repo"
+        echo "Currently working in the hippo repo"
         echo "Docker composing up Hippo with Event-Streaming"
         echo "Executing: SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml -f docker-compose-event-streaming.yml up -d --remove-orphans"
         SKIP_INSTALL=True MINIFY_JS=False docker compose -f docker-compose.yml -f docker-compose.event-streaming.yml up -d --remove-orphans
     else
-        echo "Currently working in another repo other than Hippo"
+        echo "Currently working in the the thor-web repo"
         echo "Docker composing up with docker-compose.yaml from current working directory"
         echo "Docker compose up -d --remove-orphans"
         docker compose up -d --remove-orphans
